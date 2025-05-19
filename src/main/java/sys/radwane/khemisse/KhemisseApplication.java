d2049f10d958a8bdc6599640eb86a0f67374b3b9
khemisse-radwane <khemisse.radwane@gmail.com>
2025-05-19 09:09:06 +0100

    first commit

---
+++ src/main/java/sys/radwane/khemisse/KhemisseApplication.java
@@ -0,0 +1,13 @@
+package sys.radwane.khemisse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhemisseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhemisseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sys/radwane/khemisse/KhemisseApplicationTests.java
@@ -0,0 +1,13 @@
+package sys.radwane.khemisse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhemisseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
