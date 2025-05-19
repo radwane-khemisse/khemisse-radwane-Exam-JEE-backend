17580a97bc072e2eb47f60e1bfffb07447980b53
khemisse-radwane <khemisse.radwane@gmail.com>
2025-05-19 12:00:02 +0100

    added new instances

--- src/main/java/sys/radwane/khemisse/KhemisseApplication.java
+++ src/main/java/sys/radwane/khemisse/KhemisseApplication.java
@@ -21,39 +21,121 @@ public class KhemisseApplication {
 			ClientRepository clientRepository,
 			CreditRepository creditRepository,
 			RemboursementRepository remboursementRepository) {
-		
+
 		return args -> {
-			Client client = new Client();
-			client.setName("Khemisse raddwane");
-			client.setEmail("khemisse@gmail.com");
-			clientRepository.save(client);
+			// Create multiple clients
+			Client client1 = new Client();
+			client1.setName("Radwane Khemisse");
+			client1.setEmail("radwane.khemisse@example.com");
+			clientRepository.save(client1);
 
-			CreditPersonnel creditPersonnel = new CreditPersonnel();
-			creditPersonnel.setRequestDate(new Date());
-			creditPersonnel.setStatus(CreditStatus.EN_COURS);
-			creditPersonnel.setAmount(50000.0);
-			creditPersonnel.setRepaymentDuration(24);
-			creditPersonnel.setInterestRate(4.5);
-			creditPersonnel.setMotif("Home renovation");
-			creditPersonnel.setClient(client);
-			creditRepository.save(creditPersonnel);
+			Client client2 = new Client();
+			client2.setName("Bilal Ahmed");
+			client2.setEmail("bilal.ahmed@example.com");
+			clientRepository.save(client2);
 
-			CreditImmobilier creditImmobilier = new CreditImmobilier();
-			creditImmobilier.setRequestDate(new Date());
-			creditImmobilier.setStatus(CreditStatus.ACCEPTE);
-			creditImmobilier.setAmount(250000.0);
-			creditImmobilier.setRepaymentDuration(120);
-			creditImmobilier.setInterestRate(3.2);
-			creditImmobilier.setTypeBien(TypeBien.MAISON);
-			creditImmobilier.setClient(client);
-			creditRepository.save(creditImmobilier);
+			Client client3 = new Client();
+			client3.setName("Youssef Ali");
+			client3.setEmail("youssef.ali@example.com");
+			clientRepository.save(client3);
 
-			Remboursement remboursement = new Remboursement();
-			remboursement.setDate(new Date());
-			remboursement.setAmount(1200.0);
-			remboursement.setType(RemboursementType.MENSUALITE);
-			remboursement.setCredit(creditPersonnel);
-			remboursementRepository.save(remboursement);
+			Client client4 = new Client();
+			client4.setName("Amine Hassan");
+			client4.setEmail("amine.hassan@example.com");
+			clientRepository.save(client4);
+
+			Client client5 = new Client();
+			client5.setName("Karim Mohamed");
+			client5.setEmail("karim.mohamed@example.com");
+			clientRepository.save(client5);
+
+			// Create multiple credits
+			CreditPersonnel creditPersonnel1 = new CreditPersonnel();
+			creditPersonnel1.setRequestDate(new Date());
+			creditPersonnel1.setStatus(CreditStatus.EN_COURS);
+			creditPersonnel1.setAmount(50000.0);
+			creditPersonnel1.setRepaymentDuration(24);
+			creditPersonnel1.setInterestRate(4.5);
+			creditPersonnel1.setMotif("Home renovation");
+			creditPersonnel1.setClient(client1);
+			creditRepository.save(creditPersonnel1);
+
+			CreditImmobilier creditImmobilier1 = new CreditImmobilier();
+			creditImmobilier1.setRequestDate(new Date());
+			creditImmobilier1.setStatus(CreditStatus.ACCEPTE);
+			creditImmobilier1.setAmount(250000.0);
+			creditImmobilier1.setRepaymentDuration(120);
+			creditImmobilier1.setInterestRate(3.2);
+			creditImmobilier1.setTypeBien(TypeBien.MAISON);
+			creditImmobilier1.setClient(client2);
+			creditRepository.save(creditImmobilier1);
+
+			CreditProfessionnel creditProfessionnel1 = new CreditProfessionnel();
+			creditProfessionnel1.setRequestDate(new Date());
+			creditProfessionnel1.setStatus(CreditStatus.EN_COURS);
+			creditProfessionnel1.setAmount(100000.0);
+			creditProfessionnel1.setRepaymentDuration(60);
+			creditProfessionnel1.setInterestRate(5.0);
+			creditProfessionnel1.setMotif("Business expansion");
+			creditProfessionnel1.setRaisonSociale("Khemisse Corp");
+			creditProfessionnel1.setClient(client3);
+			creditRepository.save(creditProfessionnel1);
+
+			CreditPersonnel creditPersonnel2 = new CreditPersonnel();
+			creditPersonnel2.setRequestDate(new Date());
+			creditPersonnel2.setStatus(CreditStatus.REJETE);
+			creditPersonnel2.setAmount(30000.0);
+			creditPersonnel2.setRepaymentDuration(12);
+			creditPersonnel2.setInterestRate(6.0);
+			creditPersonnel2.setMotif("Car purchase");
+			creditPersonnel2.setClient(client4);
+			creditRepository.save(creditPersonnel2);
+
+			CreditImmobilier creditImmobilier2 = new CreditImmobilier();
+			creditImmobilier2.setRequestDate(new Date());
+			creditImmobilier2.setStatus(CreditStatus.ACCEPTE);
+			creditImmobilier2.setAmount(150000.0);
+			creditImmobilier2.setRepaymentDuration(84);
+			creditImmobilier2.setInterestRate(3.5);
+			creditImmobilier2.setTypeBien(TypeBien.APPARTEMENT);
+			creditImmobilier2.setClient(client5);
+			creditRepository.save(creditImmobilier2);
+
+			// Create multiple repayments
+			Remboursement remboursement1 = new Remboursement();
+			remboursement1.setDate(new Date());
+			remboursement1.setAmount(1200.0);
+			remboursement1.setType(RemboursementType.MENSUALITE);
+			remboursement1.setCredit(creditPersonnel1);
+			remboursementRepository.save(remboursement1);
+
+			Remboursement remboursement2 = new Remboursement();
+			remboursement2.setDate(new Date());
+			remboursement2.setAmount(2500.0);
+			remboursement2.setType(RemboursementType.MENSUALITE);
+			remboursement2.setCredit(creditImmobilier1);
+			remboursementRepository.save(remboursement2);
+
+			Remboursement remboursement3 = new Remboursement();
+			remboursement3.setDate(new Date());
+			remboursement3.setAmount(5000.0);
+			remboursement3.setType(RemboursementType.REMBOURSEMENT_ANTICIPE);
+			remboursement3.setCredit(creditProfessionnel1);
+			remboursementRepository.save(remboursement3);
+
+			Remboursement remboursement4 = new Remboursement();
+			remboursement4.setDate(new Date());
+			remboursement4.setAmount(800.0);
+			remboursement4.setType(RemboursementType.MENSUALITE);
+			remboursement4.setCredit(creditPersonnel2);
+			remboursementRepository.save(remboursement4);
+
+			Remboursement remboursement5 = new Remboursement();
+			remboursement5.setDate(new Date());
+			remboursement5.setAmount(1800.0);
+			remboursement5.setType(RemboursementType.MENSUALITE);
+			remboursement5.setCredit(creditImmobilier2);
+			remboursementRepository.save(remboursement5);
 		};
 	}
 
